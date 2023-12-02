275c9742a529ddc75ee10c2ddaa8d7dfefc5fae1
Cody <codycabreraa@gmail.com>
2023-12-01 20:54:03 -0500

    Consulta

--- consulta002/ejemplo2.py
+++ consulta002/ejemplo2.py
@@ -1,18 +1,18 @@
 suma_notas = 0
 notas = []
-contador = True
+c = True
 num_notas = 0
-while contador == True:
+while c == True:
     nota = float(input(f"Ingrese la nota: "))
     notas.append(nota)
     suma_notas += nota
-    contador += 1
+    c += 1
     num_notas += 1
     print("Desea continuar? Presione x para mostrar el promedio")
     bandera = str(input())
     if bandera == "x":
-        contador = False
+        c = False
     else:
-        contador = True
+        c = True
 promedio = suma_notas / num_notas
 print(f"\nPromedio final: {promedio}")
